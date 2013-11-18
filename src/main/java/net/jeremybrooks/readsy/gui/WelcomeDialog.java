11bee36218abdaf70c4ea3088bb12f3ae3b0340c
jeremybrooks <jeremyb@whirljack.net>
2013-11-17 20:26:47 -0800

    update copyright

--- src/main/java/net/jeremybrooks/readsy/gui/WelcomeDialog.java
+++ src/main/java/net/jeremybrooks/readsy/gui/WelcomeDialog.java
@@ -1,4 +1,25 @@
 /*
+ * readsy - read something new every day
+ *
+ *     Copyright (C) 2103  Jeremy Brooks
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *     You may contact the program's author at jeremyb@whirljack.net
+ */
+
+/*
  * Created by JFormDesigner on Sat Nov 16 01:26:17 PST 2013
  */
 
