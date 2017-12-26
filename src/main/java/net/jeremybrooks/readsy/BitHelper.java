c4d5b10a532ed77a4ae52d35d60f3f948aa9d9ea
jeremybrooks <jeremyb@whirljack.net>
2017-12-26 10:06:29 -0800

    Fixed a bug where unread count could be wrong on the last day of the year. Update to install4j 7.

--- src/main/java/net/jeremybrooks/readsy/BitHelper.java
+++ src/main/java/net/jeremybrooks/readsy/BitHelper.java
@@ -30,121 +30,125 @@ import java.util.GregorianCalendar;
  */
 public class BitHelper {
 
-	private byte[] bytes;
-	private GregorianCalendar calendar;
-	private static final String EMPTY_BITSET = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+  private byte[] bytes;
+  private GregorianCalendar calendar;
+  private static final String EMPTY_BITSET = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 
-	/**
-	 * Create an instance of BitHelper with enough space to store a years worth of flags.
-	 */
-	public BitHelper() {
-		this(EMPTY_BITSET);
-	}
-
-
-	/**
-	 * Create a new instance of BitHelper with the specified hex string.
-	 *
-	 * @param hexBytes the bytes that will be managed by this instance of BitHelper.
-	 */
-	public BitHelper(String hexBytes) {
-		int len = hexBytes.length();
-		bytes = new byte[len / 2];
-		for (int i = 0; i < len; i += 2) {
-			bytes[i / 2] = (byte) ((Character.digit(hexBytes.charAt(i), 16) << 4)
-					+ Character.digit(hexBytes.charAt(i + 1), 16));
-		}
-		this.calendar = new GregorianCalendar();
-	}
-
-	/**
-	 * Determine if the flag is true or false for the specified date.
-	 *
-	 * @param date date to look up.
-	 * @return true if the flag is 1, false otherwise.
-	 */
-	public boolean isRead(Date date) {
-		int dayOfYear = this.getDayOfYear(date);
-		int mask = (int) Math.pow(2, (dayOfYear - 1) % 8);            // adjust to zero-based index
-		return ((this.bytes[whichByte(dayOfYear)]) & mask) == mask;
-	}
+  /**
+   * Create an instance of BitHelper with enough space to store a years worth of flags.
+   */
+  public BitHelper() {
+    this(EMPTY_BITSET);
+  }
 
 
-	/**
-	 * Set the value of the flag for the specified date.
-	 * <p>If the flag is already set to the specified value, nothing happens.</p>
-	 *
-	 * @param date the date to set the flag for.
-	 * @param read what the flag should be set to.
-	 */
-	public void setRead(Date date, boolean read) {
-		if (this.isRead(date) != read) {
-			int dayOfYear = this.getDayOfYear(date);
-			int result = ((int) this.bytes[whichByte(dayOfYear)]) ^ (int) Math.pow(2, (dayOfYear - 1) % 8);
-			this.bytes[whichByte(dayOfYear)] = (byte) result;
-		}
-	}
+  /**
+   * Create a new instance of BitHelper with the specified hex string.
+   *
+   * @param hexBytes the bytes that will be managed by this instance of BitHelper.
+   */
+  public BitHelper(String hexBytes) {
+    int len = hexBytes.length();
+    bytes = new byte[len / 2];
+    for (int i = 0; i < len; i += 2) {
+      bytes[i / 2] = (byte) ((Character.digit(hexBytes.charAt(i), 16) << 4)
+          + Character.digit(hexBytes.charAt(i + 1), 16));
+    }
+    this.calendar = new GregorianCalendar();
+  }
 
-	/**
-	 * Get the byte position that contains this day of the year.
-	 * Years start at 1, but the array starts at zero. This is taken into account.
-	 *
-	 * @param dayOfYear which day of the year. Years start at 1.
-	 * @return which byte position contains the day.
-	 */
-	public int whichByte(int dayOfYear) {
-		return this.bytes.length - (((this.bytes.length * 8) - dayOfYear) / 8) - 1;
-	}
-
-
-	/**
-	 * Get the ordinal day of the year for the date.
-	 *
-	 * @param date the date.
-	 * @return ordinal day of the year for the date.
-	 */
-	public int getDayOfYear(Date date) {
-		this.calendar.setTime(date);
-		return calendar.get(Calendar.DAY_OF_YEAR);
-	}
+  /**
+   * Determine if the flag is true or false for the specified date.
+   *
+   * @param date date to look up.
+   * @return true if the flag is 1, false otherwise.
+   */
+  public boolean isRead(Date date) {
+    int dayOfYear = this.getDayOfYear(date);
+    int mask = (int) Math.pow(2, (dayOfYear - 1) % 8);            // adjust to zero-based index
+    return ((this.bytes[whichByte(dayOfYear)]) & mask) == mask;
+  }
 
 
-	/**
-	 *
-	 * @param stopDate the date to stop looking for unread items, inclusive.
-	 * @param year the year that the data file is good for, "0" for all years.
-	 * @return number of unread items from Jan 1 to stopDate.
-	 */
-	public int getUnreadItemCount(Date stopDate, String year) {
-		int count = 0;
+  /**
+   * Set the value of the flag for the specified date.
+   * <p>If the flag is already set to the specified value, nothing happens.</p>
+   *
+   * @param date the date to set the flag for.
+   * @param read what the flag should be set to.
+   */
+  public void setRead(Date date, boolean read) {
+    if (this.isRead(date) != read) {
+      int dayOfYear = this.getDayOfYear(date);
+      int result = ((int) this.bytes[whichByte(dayOfYear)]) ^ (int) Math.pow(2, (dayOfYear - 1) % 8);
+      this.bytes[whichByte(dayOfYear)] = (byte) result;
+    }
+  }
 
-		Calendar cal = new GregorianCalendar();
-		cal.setTime(stopDate);
-		int stopDay = cal.get(Calendar.DAY_OF_YEAR);
-		if (year.equals("0") || year.equals(Integer.toString(cal.get(Calendar.YEAR)))) {
-			Calendar theDay = new GregorianCalendar();
-			theDay.set(Calendar.DAY_OF_YEAR, 1);
-			while (theDay.get(Calendar.DAY_OF_YEAR) <= stopDay) {
-				if (!isRead(theDay.getTime())) {
-					count++;
-				}
-				theDay.add(Calendar.DAY_OF_YEAR, 1);
-			}
-		}
-		return count;
-	}
+  /**
+   * Get the byte position that contains this day of the year.
+   * Years start at 1, but the array starts at zero. This is taken into account.
+   *
+   * @param dayOfYear which day of the year. Years start at 1.
+   * @return which byte position contains the day.
+   */
+  public int whichByte(int dayOfYear) {
+    return this.bytes.length - (((this.bytes.length * 8) - dayOfYear) / 8) - 1;
+  }
 
-	/**
-	 * Return the byte values as a hex string.
-	 *
-	 * @return current byte values as a hex string.
-	 */
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < this.bytes.length; i++) {
-			sb.append(String.format("%02x", this.bytes[i]));
-		}
-		return sb.toString();
-	}
+
+  /**
+   * Get the ordinal day of the year for the date.
+   *
+   * @param date the date.
+   * @return ordinal day of the year for the date.
+   */
+  public int getDayOfYear(Date date) {
+    this.calendar.setTime(date);
+    return calendar.get(Calendar.DAY_OF_YEAR);
+  }
+
+
+  /**
+   * Count how many unread items there are from the beginning of the year until
+   * stopDate, inclusive.
+   *
+   * @param stopDate the date to stop looking for unread items, inclusive.
+   * @param year     the year that the data file is good for, "0" for all years.
+   * @return number of unread items from Jan 1 to stopDate.
+   */
+  public int getUnreadItemCount(Date stopDate, String year) {
+    int count = 0;
+
+    Calendar cal = new GregorianCalendar();
+    cal.setTime(stopDate);
+    int stopDay = cal.get(Calendar.DAY_OF_YEAR);
+    if (year.equals("0") || year.equals(Integer.toString(cal.get(Calendar.YEAR)))) {
+      Calendar theDay = new GregorianCalendar();
+      theDay.set(Calendar.DAY_OF_YEAR, 1);
+      do {
+        if (!isRead(theDay.getTime())) {
+          count++;
+        }
+        theDay.add(Calendar.DAY_OF_YEAR, 1);
+      }
+      while (theDay.get(Calendar.DAY_OF_YEAR) <= stopDay && theDay.get(Calendar.DAY_OF_YEAR) != 1);
+      // note: the != 1 ensures that, if stop date is 12/31, we stop after one time through the year
+    }
+    return count;
+  }
+
+  /**
+   * Return the byte values as a hex string.
+   *
+   * @return current byte values as a hex string.
+   */
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < this.bytes.length; i++) {
+      sb.append(String.format("%02x", this.bytes[i]));
+    }
+    return sb.toString();
+  }
 }
--- src/test/java/net/whirljack/sdj/BitHelperTest.java
+++ src/test/java/net/whirljack/sdj/BitHelperTest.java
@@ -38,13 +38,13 @@ import static org.junit.Assert.assertTrue;
  * @author Jeremy Brooks
  */
 public class BitHelperTest {
-	private static final String testInit = "000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f7772bc98aaff12450000dddbce65";
+  private static final String testInit = "000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f7772bc98aaff12450000dddbce65";
 
-	private static final String testNothingRead = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+  private static final String testNothingRead = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 
-	@Test
+  @Test
   public void test() throws Exception {
-	  SimpleDateFormat sdf = new SimpleDateFormat("MMdd");
+    SimpleDateFormat sdf = new SimpleDateFormat("MMdd");
     Calendar cal = new GregorianCalendar();
     cal.set(Calendar.MONTH, 0);
     cal.set(Calendar.DAY_OF_MONTH, 1);
@@ -54,114 +54,129 @@ public class BitHelperTest {
     }
 
   }
-	@Test
-	public void testWhichByte() throws Exception {
-		// expected is an array with 8 0's, 8 1's, etc.
-		int[] expected = new int[368];
-		int x = 0;
-		for (int i = 0; i < expected.length; i += 8) {
-			for (int j = 0; j < 8; j++) {
-				expected[i + j] = x;
-			}
-			x++;
-		}
-		BitHelper bitHelper = new BitHelper(testInit);
-		for (int i = 1; i < 367; i++) {
-			assertEquals(expected[i - 1], bitHelper.whichByte(i));
-		}
-	}
+
+  @Test
+  public void testWhichByte() throws Exception {
+    // expected is an array with 8 0's, 8 1's, etc.
+    int[] expected = new int[368];
+    int x = 0;
+    for (int i = 0; i < expected.length; i += 8) {
+      for (int j = 0; j < 8; j++) {
+        expected[i + j] = x;
+      }
+      x++;
+    }
+    BitHelper bitHelper = new BitHelper(testInit);
+    for (int i = 1; i < 367; i++) {
+      assertEquals(expected[i - 1], bitHelper.whichByte(i));
+    }
+  }
 
 
-	@Test
-	public void testRandomBits() throws Exception {
-		SecureRandom random = new SecureRandom();
-		BitHelper bitHelper = new BitHelper(testNothingRead);
-		boolean flag = true;
-		Calendar cal = new GregorianCalendar();
-		for (int i = 0; i < 2000; i++) {
-			int dayOfYear = random.nextInt(365) + 1;
-			cal.set(Calendar.DAY_OF_YEAR, dayOfYear);
-			bitHelper.setRead(cal.getTime(), flag);
-			assertEquals(flag, bitHelper.isRead(cal.getTime()));
-			flag = !flag;
-		}
-	}
+  @Test
+  public void testRandomBits() throws Exception {
+    SecureRandom random = new SecureRandom();
+    BitHelper bitHelper = new BitHelper(testNothingRead);
+    boolean flag = true;
+    Calendar cal = new GregorianCalendar();
+    for (int i = 0; i < 2000; i++) {
+      int dayOfYear = random.nextInt(365) + 1;
+      cal.set(Calendar.DAY_OF_YEAR, dayOfYear);
+      bitHelper.setRead(cal.getTime(), flag);
+      assertEquals(flag, bitHelper.isRead(cal.getTime()));
+      flag = !flag;
+    }
+  }
 
-	@Test
-	public void testStrings() throws Exception {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-		String expectedJan1 = "01000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+  @Test
+  public void testStrings() throws Exception {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+    String expectedJan1 = "01000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 
-		BitHelper bitHelper = new BitHelper(testNothingRead);
-		bitHelper.setRead(sdf.parse("20130101"), true);
-		assertEquals(expectedJan1, bitHelper.toString());
+    BitHelper bitHelper = new BitHelper(testNothingRead);
+    bitHelper.setRead(sdf.parse("20130101"), true);
+    assertEquals(expectedJan1, bitHelper.toString());
 
-		bitHelper = new BitHelper(testInit);
-		assertEquals(testInit, bitHelper.toString());
-	}
+    bitHelper = new BitHelper(testInit);
+    assertEquals(testInit, bitHelper.toString());
+  }
 
-	@Test
-	public void testAllRead() throws Exception {
-		String all = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
-		BitHelper bitHelper = new BitHelper(all);
-		Calendar cal = new GregorianCalendar();
-		for (int i = 1; i < 366; i++) {
-			cal.set(Calendar.DAY_OF_YEAR, i);
-			assertTrue(bitHelper.isRead(cal.getTime()));
-		}
-	}
+  @Test
+  public void testAllRead() throws Exception {
+    String all = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+    BitHelper bitHelper = new BitHelper(all);
+    Calendar cal = new GregorianCalendar();
+    for (int i = 1; i < 366; i++) {
+      cal.set(Calendar.DAY_OF_YEAR, i);
+      assertTrue(bitHelper.isRead(cal.getTime()));
+    }
+  }
 
-	@Test
-	public void testNoneRead() throws Exception {
-		String none = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-		BitHelper bitHelper = new BitHelper(none);
-		Calendar cal = new GregorianCalendar();
-		for (int i = 1; i < 366; i++) {
-			cal.set(Calendar.DAY_OF_YEAR, i);
-			assertFalse(bitHelper.isRead(cal.getTime()));
-		}
-	}
+  @Test
+  public void testNoneRead() throws Exception {
+    String none = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+    BitHelper bitHelper = new BitHelper(none);
+    Calendar cal = new GregorianCalendar();
+    for (int i = 1; i < 366; i++) {
+      cal.set(Calendar.DAY_OF_YEAR, i);
+      assertFalse(bitHelper.isRead(cal.getTime()));
+    }
+  }
 
-	/*
-	 * Creates a read string for a known number of read days. Used to test other platform compatibility.
-	 */
-	@Test
-	public void createTestString() throws Exception {
-		int[] days = {3,9,35,76,138,166,210,248,299,309,333,365};
-		String none = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-		BitHelper bitHelper = new BitHelper(none);
-		Calendar cal = new GregorianCalendar();
-		for (int i : days) {
-			cal.set(Calendar.DAY_OF_YEAR, i);
-			bitHelper.setRead(cal.getTime(), true);
-		}
-	}
+  /*
+   * Creates a read string for a known number of read days. Used to test other platform compatibility.
+   */
+  @Test
+  public void createTestString() throws Exception {
+    int[] days = {3, 9, 35, 76, 138, 166, 210, 248, 299, 309, 333, 365};
+    String none = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+    BitHelper bitHelper = new BitHelper(none);
+    Calendar cal = new GregorianCalendar();
+    for (int i : days) {
+      cal.set(Calendar.DAY_OF_YEAR, i);
+      bitHelper.setRead(cal.getTime(), true);
+    }
+  }
 
-	@Test
-	public void testGetUnreadItemCount() throws Exception {
-		// this string has unread items at Nov 23, and Nov 25 to end of the year for year 2013
-		String read = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbf0000000000";
-		BitHelper bitHelper = new BitHelper(read);
-		Calendar stopDate = new GregorianCalendar();
-		stopDate.set(Calendar.YEAR, 2013);
-		stopDate.set(Calendar.MONTH, 10);
-		stopDate.set(Calendar.DAY_OF_MONTH, 25);
-		int count = bitHelper.getUnreadItemCount(stopDate.getTime(), "2013");
-		assertEquals(2, count);
-		count = bitHelper.getUnreadItemCount(stopDate.getTime(), "0");
-		assertEquals(2, count);
-	}
+  @Test
+  public void testGetUnreadItemCount() throws Exception {
+    // this string has unread items at Nov 23, and Nov 25 to end of the year for year 2013
+    String read = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbf0000000000";
+    BitHelper bitHelper = new BitHelper(read);
+    Calendar stopDate = new GregorianCalendar();
+    stopDate.set(Calendar.YEAR, 2013);
+    stopDate.set(Calendar.MONTH, 10);
+    stopDate.set(Calendar.DAY_OF_MONTH, 25);
+    int count = bitHelper.getUnreadItemCount(stopDate.getTime(), "2013");
+    assertEquals(2, count);
+    count = bitHelper.getUnreadItemCount(stopDate.getTime(), "0");
+    assertEquals(2, count);
+  }
 
-	@Test
-		public void testGetUnreadItemCountInvalidYear() throws Exception {
-			// this string has unread items at Nov 23, and Nov 25 to end of the year for year 2013
-			String read = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbf0000000000";
-			BitHelper bitHelper = new BitHelper(read);
-			Calendar stopDate = new GregorianCalendar();
-			stopDate.set(Calendar.YEAR, 2012);
-			stopDate.set(Calendar.MONTH, 10);
-			stopDate.set(Calendar.DAY_OF_MONTH, 25);
-			int count = bitHelper.getUnreadItemCount(stopDate.getTime(), "2013");
-			assertEquals(0, count);
-		}
+  @Test
+  public void testGetUnreadItemCountInvalidYear() throws Exception {
+    // this string has unread items at Nov 23, and Nov 25 to end of the year for year 2013
+    String read = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbf0000000000";
+    BitHelper bitHelper = new BitHelper(read);
+    Calendar stopDate = new GregorianCalendar();
+    stopDate.set(Calendar.YEAR, 2012);
+    stopDate.set(Calendar.MONTH, 10);
+    stopDate.set(Calendar.DAY_OF_MONTH, 25);
+    int count = bitHelper.getUnreadItemCount(stopDate.getTime(), "2013");
+    assertEquals(0, count);
+  }
+
+  @Test
+  public void getUnreadItemCountEndOfYear() throws Exception {
+    // this bit pattern is all days read up to dec 22, so there should be 9 unread days
+    BitHelper bitHelper = new BitHelper("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0f00");
+
+    // make a calendar for the last day of the year
+    GregorianCalendar dec31 = new GregorianCalendar();
+    dec31.set(GregorianCalendar.YEAR, 2017);
+    int currentDayOfYear = dec31.get(Calendar.DAY_OF_YEAR);
+    dec31.set(GregorianCalendar.MONTH, GregorianCalendar.DECEMBER);
+    dec31.set(GregorianCalendar.DAY_OF_MONTH, 31);
+    assertEquals(9, bitHelper.getUnreadItemCount(dec31.getTime(), "2017"));
+  }
 }
